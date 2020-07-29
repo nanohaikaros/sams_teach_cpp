ee1cb277b262c0b072962d68f1d4e57b0b6cfe33
Nanoha <1371689491@qq.com>
2020-07-29 12:20:48 +0800

    1

---
+++ 3/1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "This program will help you multiply two numbers" << endl;
+    int firstNumber = 0;
+    cin >> firstNumber;
+
+    cout << "Enter the secone number: ";
+    int secondNumber = 0;
+    cin >> secondNumber;
+
+    // Multiply two numbers, store result in a variable
+    int multiplicationResult = firstNumber * secondNumber;
+
+    // Display result
+    cout << firstNumber << " x " << secondNumber;
+    cout << " = " << multiplicationResult << endl;
+
+    return 0;
+}
