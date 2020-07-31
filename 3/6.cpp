eb276be67b09b1ae65556bb4074976cad07be44f
Nanoha <1371689491@qq.com>
2020-07-31 17:53:38 +0800

    1

---
+++ 3/6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    auto coinFlippedHeads = true;
+    auto largeNumber = 2500000000000;
+
+    cout << "coinFlippedHeads = " << coinFlippedHeads;
+    cout << " , sizeof(coinFlippedHeads) = " << sizeof(coinFlippedHeads) << endl;
+
+    cout << "largeNumber = " << largeNumber;
+    cout << " , sizeof(largeNumber) = " << sizeof(largeNumber) << endl;
+
+    return 0;
+}
